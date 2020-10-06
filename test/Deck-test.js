1cef38f3eae1458b699ff4957a892acb6a8b8762
Peyton Jones <peytonj.dev@gmail.com>
2020-10-05 20:43:17 -0600

    Add tests and method for countCards

--- test/Deck-test.js
+++ test/Deck-test.js
@@ -22,6 +22,16 @@ describe('Deck', function() {
 
         expect(deck.cards).to.equal(cards)
     })
-    
-    // return a count of the cards
+
+    it('should return a count of the cards', function() {
+        const card1 = new Card(1, 'What is Robbie\'s favorite animal', ['sea otter', 'pug', 'capybara'], 'sea otter');
+        const card2 = new Card(14, 'What organ is Khalid missing?', ['spleen', 'appendix', 'gallbladder'], 'gallbladder');
+        const card3 = new Card(12, 'What is Travis\'s middle name?', ['Lex', 'William', 'Fitzgerald'], 'Fitzgerald');
+        const cards = [card1, card2, card3]
+        const deck = new Deck(cards)
+
+        const count = deck.countCards()
+
+        expect(count).to.equal(3)
+    })
 })
