4edbf1460534f960d5c5ef0ef9d2ed66a6115ab5
Peyton Jones <peytonj.dev@gmail.com>
2020-10-05 20:38:58 -0600

    Add tests and property for cards

--- test/Deck-test.js
+++ test/Deck-test.js
@@ -1,4 +1,5 @@
 const { expect } = require("chai")
+const Card = require("../src/Card")
 const Deck = require("../src/Deck")
 
 describe('Deck', function() {
@@ -11,4 +12,16 @@ describe('Deck', function() {
         const deck = new Deck()
         expect(deck).to.be.an.instanceof(Deck);
     })
+
+    it('should take in an array of cards', function() {
+        const card1 = new Card(1, 'What is Robbie\'s favorite animal', ['sea otter', 'pug', 'capybara'], 'sea otter');
+        const card2 = new Card(14, 'What organ is Khalid missing?', ['spleen', 'appendix', 'gallbladder'], 'gallbladder');
+        const card3 = new Card(12, 'What is Travis\'s middle name?', ['Lex', 'William', 'Fitzgerald'], 'Fitzgerald');
+        const cards = [card1, card2, card3]
+        const deck = new Deck(cards)
+
+        expect(deck.cards).to.equal(cards)
+    })
+    
+    // return a count of the cards
 })
