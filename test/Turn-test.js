a6f2ba4b63fe20ee6d253c6118dbddde56f870d8
Peyton Jones <peytonj.dev@gmail.com>
2020-10-05 19:58:58 -0600

    Remove comments

--- test/Turn-test.js
+++ test/Turn-test.js
@@ -1,14 +1,8 @@
-// returnGuess: method that returns the guess
-// returnCard: method that returns the Card
-// evaluateGuess: method that returns a boolean indicating if the user’s guess matches the correct answer on the card
-// giveFeedback - method that returns either ‘incorrect!’ or ‘correct!’ based on whether the guess is correct or not.
-
 const chai = require('chai');
 const Card = require('../src/Card');
 const expect = chai.expect;
 
 const Turn = require('../src/Turn');
-
 
 describe('Turn', function() {
   it('should be a class', function() { 
