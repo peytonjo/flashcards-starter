595fb89657269bdcaf6b21b39383534093e42343
Peyton Jones <peytonj.dev@gmail.com>
2020-10-07 14:42:07 -0600

    Remove comments from round test

--- test/Round-test.js
+++ test/Round-test.js
@@ -3,19 +3,6 @@ const Card = require("../src/Card");
 const Deck = require("../src/Deck");
 const Round = require("../src/Round");
 
-
-// Your Round class should meet the following requirements:
-// returnCurrentCard: method that returns the current card being played
-// takeTurn: method that updates turns count, evaluates guesses, gives feedback, and stores ids of incorrect guesses
-
-// When a guess is made, a new Turn instance is created.
-// The turns count is updated, regardless of whether the guess is correct or incorrect
-// The next card becomes current card
-// Guess is evaluated/recorded. Incorrect guesses will be stored (via the id) in an array of incorrectGuesses
-// Feedback is returned regarding whether the guess is incorrect or correct
-// calculatePercentCorrect: method that calculates and returns the percentage of correct guesses
-// endRound: method that prints the following to the console: ‘** Round over! ** You answered <>% of the questions correctly!`
-
 describe('Round', function () {
   it('should be a class', function () {
     const round = new Round();
@@ -39,14 +26,7 @@ describe('Round', function () {
 
     expect(currentCard).to.equal(card1)
   })
-  // takeTurn: method that updates turns count, evaluates guesses, 
-  // gives feedback, and stores card id of incorrect guesses
 
-  // round.turns 0
-  // round.incorrectGuesses []
-  // round.takeTurn(guess)
-  // updateTurnCount
-  // guess id?
   it('should update turn count', function() {
     const card1 = new Card(1, 'What is Robbie\'s favorite animal', ['sea otter', 'pug', 'capybara'], 'sea otter');
     const card2 = new Card(14, 'What organ is Khalid missing?', ['spleen', 'appendix', 'gallbladder'], 'gallbladder');
