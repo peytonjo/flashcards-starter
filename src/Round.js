48ed37219f9592c84b9ddcac2a091f1c492f9171
Peyton Jones <peytonj.dev@gmail.com>
2020-10-06 20:21:44 -0600

    Add tests and method for takeTurn

--- src/Round.js
+++ src/Round.js
@@ -1,10 +1,29 @@
+const Turn = require("../src/Turn");
+
 class Round {
   constructor(deck) {
     this.deck = deck
+    this.turns = 0
+    this.incorrectGuesses = []
   }
 
   returnCurrentCard() {
     return this.deck.cards[0]
+  }
+
+  takeTurn(guess) {
+    this.turns = this.turns + 1
+  
+    const currentCard = this.returnCurrentCard()
+    const turn = new Turn(guess, currentCard)
+
+    if(!turn.evaluateGuess()) {
+      this.incorrectGuesses.push(currentCard.id)
+    }
+   
+    const feedback = turn.giveFeedback()
+    
+    return feedback
   }
 }
 
--- test/Round-test.js
+++ test/Round-test.js
@@ -39,4 +39,72 @@ describe('Round', function () {
 
     expect(currentCard).to.equal(card1)
   })
+  // takeTurn: method that updates turns count, evaluates guesses, 
+  // gives feedback, and stores card id of incorrect guesses
+
+  // round.turns 0
+  // round.incorrectGuesses []
+  // round.takeTurn(guess)
+  // updateTurnCount
+  // guess id?
+  it('should update turn count', function() {
+    const card1 = new Card(1, 'What is Robbie\'s favorite animal', ['sea otter', 'pug', 'capybara'], 'sea otter');
+    const card2 = new Card(14, 'What organ is Khalid missing?', ['spleen', 'appendix', 'gallbladder'], 'gallbladder');
+    const card3 = new Card(12, 'What is Travis\'s middle name?', ['Lex', 'William', 'Fitzgerald'], 'Fitzgerald');
+    const cards = [card1, card2, card3]
+    const deck = new Deck(cards)
+    const round = new Round(deck);
+
+    expect(round.turns).to.equal(0)
+
+    round.takeTurn('guess')
+
+    expect(round.turns).to.equal(1)
+  })
+
+  it('should store the id of incorrect guesses', function() {
+    const card1 = new Card(1, 'What is Robbie\'s favorite animal', ['sea otter', 'pug', 'capybara'], 'sea otter');
+    const card2 = new Card(14, 'What organ is Khalid missing?', ['spleen', 'appendix', 'gallbladder'], 'gallbladder');
+    const card3 = new Card(12, 'What is Travis\'s middle name?', ['Lex', 'William', 'Fitzgerald'], 'Fitzgerald');
+    const cards = [card1, card2, card3]
+    const deck = new Deck(cards)
+    const round = new Round(deck);
+
+    expect(round.incorrectGuesses.length).to.equal(0)
+
+    round.takeTurn('pug')
+
+    expect(round.incorrectGuesses.length).to.equal(1)
+    expect(round.incorrectGuesses[0]).to.equal(card1.id)
+  })
+
+  it('should not store the id of correct guesses', function() {
+    const card1 = new Card(1, 'What is Robbie\'s favorite animal', ['sea otter', 'pug', 'capybara'], 'sea otter');
+    const card2 = new Card(14, 'What organ is Khalid missing?', ['spleen', 'appendix', 'gallbladder'], 'gallbladder');
+    const card3 = new Card(12, 'What is Travis\'s middle name?', ['Lex', 'William', 'Fitzgerald'], 'Fitzgerald');
+    const cards = [card1, card2, card3]
+    const deck = new Deck(cards)
+    const round = new Round(deck);
+
+    expect(round.incorrectGuesses.length).to.equal(0)
+
+    round.takeTurn('sea otter')
+
+    expect(round.incorrectGuesses.length).to.equal(0)
+  })
+
+  it('should return feedback on if the guess is correct or not', function() {
+    const card1 = new Card(1, 'What is Robbie\'s favorite animal', ['sea otter', 'pug', 'capybara'], 'sea otter');
+    const card2 = new Card(14, 'What organ is Khalid missing?', ['spleen', 'appendix', 'gallbladder'], 'gallbladder');
+    const card3 = new Card(12, 'What is Travis\'s middle name?', ['Lex', 'William', 'Fitzgerald'], 'Fitzgerald');
+    const cards = [card1, card2, card3]
+    const deck = new Deck(cards)
+    const round = new Round(deck);
+
+    expect(round.incorrectGuesses.length).to.equal(0)
+
+    const feedback = round.takeTurn('sea otter')
+
+    expect(feedback).to.equal('correct!')
+  })
 })
